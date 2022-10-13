3ba31b27531cf1adb355d84dac9453380d501c49
Ysh-rael_linux <ysp.rael@gmail.com>
2022-10-12 21:21:33 -0300

    tag-incremento/zerar(caso exista); formatação num

--- card.js
+++ card.js
@@ -34,6 +34,7 @@ function Drop
         pageHtml = false,
         classOver = 'o_v_e_r',
         is_draging = '.is-draging',
+        tags = '#tags',
         essentialFunctions = false
     ) {
 
@@ -42,6 +43,11 @@ function Drop
     let dropzones = document.querySelectorAll(element)
     const _cards = document.querySelector(initialZone)
     const discard = document.querySelector(to_discard)
+
+    const boxTags = document.querySelector(tags)
+    const div = () => document.createElement('div')
+
+
 
 
     const { addSan, removeSan } = essentialFunctions
@@ -53,9 +59,20 @@ function Drop
 
 
     dropzones.forEach(dropzone => {
-        dropzone.addEventListener('dragenter', dragenter)
-        dropzone.addEventListener('dragover', dragover)
-        dropzone.addEventListener('dragleave', dragleave)
+        if (dropzone !== _cards && dropzone !== discard) {
+            dropzone.addEventListener('dragenter', dragenter)
+            dropzone.addEventListener('dragover', dragover)
+            dropzone.addEventListener('dragleave', dragleave)
+            if (!dropzone.hasAttribute('number_of_cards')) dropzone.setAttribute('number_of_cards', 0)
+            else {
+                number_of_cards = Number.parseInt(dropzone.getAttribute('number_of_cards'))
+                dropzone.setAttribute('number_of_cards', number_of_cards + 1)
+            }
+
+            tag(div, boxTags, formatLargeNumbers, dropzone)
+
+        }
+
     })
 
     function dragenter() {
@@ -89,45 +106,116 @@ function Drop
 
 
 
-    
-    function addSan(dad = this, san = false) {
-        if (san) {
-            try {
-                dad.append(san)
-                return dad
-            } catch (err) {
-                console.log("Could not add child in parent tag: \n", err)
-                
-                return false
-            }
-    
-        }
-        
-    }
-    function removeSan(dad = this, san = false) {
-        if (san) {
-            try {
-                dad.remove(san)
-                return dad
-            } catch (err) {
-                console.log("Unable to remove child in parent tag: \n", err)
-                return false
-            }
-        }
-        
-    }
-    
-    
-    
-    Drag('#atual_card', false);
 
-    
-    Drop(
-        ".dropzone_tags",
-        '.cards',
-        "#to_discard", 
-        false,
-        'over',
-        '.is-dragging',
-        { addSan, removeSan }
-        )
+function addSan(dad = this, san = false) {
+    if (san) {
+        try {
+            dad.append(san)
+            return dad
+        } catch (err) {
+            console.log("Could not add child in parent tag: \n", err)
+
+            return false
+        }
+
+    }
+
+}
+function removeSan(dad = this, san = false) {
+    if (san) {
+        try {
+            dad.remove(san)
+            return dad
+        } catch (err) {
+            console.log("Unable to remove child in parent tag: \n", err)
+            return false
+        }
+    }
+
+}
+
+const tag = (div, dad, formatLargeNumbers = false, dZone = this) => {
+    const number_of_cards = dZone.getAttribute('number_of_cards')
+    const __tag = div()
+    const title = div()
+    const length = div()
+    title.classList.add('title_tag')
+    length.classList.add('length_tag')
+    __tag.classList.add('tag')
+    title.innerHTML = "<nobr>00_título_00</nobr>"
+    length.innerText = formatLargeNumbers ? formatLargeNumbers(number_of_cards) : number_of_cards
+
+
+    addSan(__tag, title)
+    addSan(__tag, length)
+    addSan(dad, __tag)
+    return __tag
+}
+
+const formatLargeNumbers = number => {
+    stng_number = Number.parseInt(number).toString()
+    function formatNumber(numberTest, amount, _start, _end) {
+        stng_number[numberTest] == 0 ?
+            stng_number = `${stng_number.substr(_start, _end)}${amount}` : stng_number = `${stng_number.substr(_start, _end)},${stng_number[_end]}${amount}`
+    }
+    switch (stng_number.length) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            break
+        case 4:
+            formatNumber(1, 'K', 0, 1)
+            break
+        case 5:
+            formatNumber(2, 'K', 0, 2)
+            break
+        case 6:
+            formatNumber(3, 'K', 0, 3)
+            break
+        case 7:
+            formatNumber(1, 'M', 0, 1)
+            break
+        case 8:
+            formatNumber(2, 'M', 0, 2)
+            break
+        case 9:
+            formatNumber(3, 'M', 0, 3)
+            break
+        case 10:
+            formatNumber(1, 'B', 0, 1)
+            break
+        case 11:
+            formatNumber(2, 'B', 0, 2)
+
+            break
+        case 12:
+            formatNumber(3, 'B', 0, 3)
+            break
+        default:
+            stng_number = '...'
+            break
+
+    }
+
+
+
+    return stng_number
+}
+
+
+
+
+Drag('#atual_card', false);
+
+
+Drop(
+    ".dropzone_tags",
+    '.cards',
+    "#to_discard",
+    false,
+    'over',
+    '.is-dragging',
+    "#tags",
+    { addSan, removeSan, tag }
+)
