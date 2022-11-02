dce42927d86e5a81633a44ce3053a51104efe1fb
Ysh-rael_linux <ysp.rael@gmail.com>
2022-11-02 18:20:23 -0300

    resolvido bug - not drag the card. remov 2 eventos

--- js/card.js
+++ js/card.js
@@ -26,23 +26,24 @@ function Drag(element, pageHtml = false, essentialFunctions = false, ...func) {
 
 
     // CARD
-    card.addEventListener('dragstart', dragstart)
-    card.addEventListener('dragend', dragend)
     card.addEventListener('touchmove', touchmove)
     card.addEventListener('touchend', touchend)
-
-    function dragstart() {
+    
+    /*  
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('dragend', dragend)
+     function dragstart() {
         // dropzones.forEach(dropzone => dropzone.classList.add('highlight')) """ para dar um estilo a todas as dropzones """
 
         // this = card
-        this.classList.add('is-dragging')
     }
     function dragend() {
         // dropzones.forEach(dropzone => dropzone.classList.remove('highlight')) 
 
         // this = card
-        this.classList.remove('is-dragging')
-    }
+    } 
+    */
+
     function touchmove(e) {
         this.classList.add('is-dragging')
 
